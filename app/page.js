b386f08cfdef2d7fd0890514fdd98bbd8f75543d
Op-Pautu <pautuop@gmail.com>
2024-02-09 14:17:08 +0530

    Fetch all products from api

--- app/page.js
+++ app/page.js
@@ -1,3 +1,46 @@
+"use client";
+
+import Product from "@/components/Product";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <div>test</div>;
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const fetchProducts = () => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => {
+        const data = res.data;
+        setProducts(data.products);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="flex w-full mx-auto bg-zinc-100 flex-col h-full">
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <div className="p-16 grid grid-cols-4 gap-8 max-w-[1600px] bg-slate-100 mx-auto">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex w-full mx-auto bg-zinc-100 flex-col h-full pt-4">
+      <h1 className="text-center text-5xl">Products</h1>
+    </div>
+  );
+};
+
+export default Navbar;
